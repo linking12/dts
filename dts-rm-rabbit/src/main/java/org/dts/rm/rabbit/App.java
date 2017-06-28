6eaecbfcb5c1068e9e936d0e31119ee65f39fec1
liushiming <liushiming@dianrong.com>
2017-06-28 13:54:13 +0800

    init project

---
+++ dts-rm-rabbit/src/main/java/org/dts/rm/rabbit/App.java
@@ -0,0 +1,13 @@
+package org.dts.rm.rabbit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
