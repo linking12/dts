361e09ee042fd8a0e325db7a24c21ed2c87732e1
shiming.liu <shiming.liu@dianrong.com>
2017-11-02 10:10:01 +0800

    update

--- dts-example/dts-client-example/src/main/java/io/dts/example/client/ClientExmaple.java
+++ dts-example/dts-client-example/src/main/java/io/dts/example/client/ClientExmaple.java
@@ -1,14 +1,5 @@
 package io.dts.example.client;
 
-import java.util.Collections;
-
-import io.dts.client.DefaultDtsTransactionManager;
-import io.dts.client.exception.DtsTransactionException;
-import io.dts.client.remoting.DtsRemotingClient;
-import io.dts.client.remoting.sender.DtsClientMessageSenderImpl;
-import io.dts.common.context.DtsContext;
-import io.dts.remoting.netty.NettyClientConfig;
-
 /**
  * Created by guoyubo on 2017/9/26.
  */
@@ -16,23 +7,6 @@ public class ClientExmaple {
 
 
   public static void main(String[] args) {
-    NettyClientConfig nettyClientConfig = new NettyClientConfig();
-    nettyClientConfig.setConnectTimeoutMillis(3000);
-    DtsRemotingClient dtsClient = new DtsRemotingClient(nettyClientConfig, Collections.singletonList("127.0.0.1:10086"));
-//     dtsClient.setAddressManager(new ZookeeperAddressManager("localhost:2181", "/dts"));
-//     dtsClient.setGroup("Default");
-//     dtsClient.setAppName("Demo");
-    dtsClient.start();
-    DtsClientMessageSenderImpl clientMessageSender = new DtsClientMessageSenderImpl(dtsClient);
-    try {
-      DefaultDtsTransactionManager transactionManager =
-          new DefaultDtsTransactionManager(clientMessageSender);
-      transactionManager.begin(3000L);
-      System.out.println(DtsContext.getCurrentXid());
-    } catch (DtsTransactionException e) {
-      e.printStackTrace();
-    } finally {
-      dtsClient.shutdown();
-    }
+
   }
 }
