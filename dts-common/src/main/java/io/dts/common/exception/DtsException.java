b188f90f38a7188167796be67cc43019a9aa8f7b
shiming.liu <shiming.liu@dianrong.com>
2017-11-08 16:39:22 +0800

    update

--- dts-common/src/main/java/io/dts/common/exception/DtsException.java
+++ dts-common/src/main/java/io/dts/common/exception/DtsException.java
@@ -3,39 +3,36 @@ package io.dts.common.exception;
 
 public class DtsException extends RuntimeException {
 
+  private static final long serialVersionUID = 1L;
 
-	private int result;
+  private int result;
 
-	/**
-	 * default for deserialize
-	 */
-	public DtsException() {
-	}
+  public DtsException() {}
 
-	public DtsException(int result, String msg) {
-		super(msg);
-		this.setResult(result);
-	}
+  public DtsException(int result, String msg) {
+    super(msg);
+    this.setResult(result);
+  }
 
-	public DtsException(String msg) {
-		this(1, msg);
-	}
+  public DtsException(String msg) {
+    this(1, msg);
+  }
 
-	public DtsException(Throwable th) {
-		super(th);
-		this.result = 0;
-	}
+  public DtsException(Throwable th) {
+    super(th);
+    this.result = 0;
+  }
 
-	public DtsException(Throwable th, String msg) {
-		super(msg, th);
-		this.result = 0;
-	}
+  public DtsException(Throwable th, String msg) {
+    super(msg, th);
+    this.result = 0;
+  }
 
-	public int getResult() {
-		return result;
-	}
+  public int getResult() {
+    return result;
+  }
 
-	public void setResult(int result) {
-		this.result = result;
-	}
+  public void setResult(int result) {
+    this.result = result;
+  }
 }
