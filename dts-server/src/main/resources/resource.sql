cb0fb2065a7b9684be16f5e259f8f196766f2b86
shiming.liu <shiming.liu@dianrong.com>
2017-11-09 11:20:07 +0800

    update script

---
+++ dts-server/src/main/resources/resource.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `txc_undo_log` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `xid` char(64) NOT NULL,
+  `branch_id` bigint(20) NOT NULL,
+  `rollback_info` longblob,
+  `gmt_create` datetime DEFAULT NULL,
+  `gmt_modified` datetime DEFAULT NULL,
+  `status` int(10) DEFAULT NULL,
+  `server` char(32) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `branch_id` (`branch_id`,`xid`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
--- dts-server/src/main/resources/server.sql
+++ dts-server/src/main/resources/server.sql
@@ -1,63 +1,44 @@
-SET NAMES utf8;
-SET FOREIGN_KEY_CHECKS=0;
--- ----------------------------
--- Table structure for txc_global_log
--- ----------------------------
-CREATE TABLE `txc_global_log` (
-  `tx_id` bigint(20) NOT NULL auto_increment,
-  `state` tinyint(1) NOT NULL,
-  `gmt_created` datetime NOT NULL,
-  `gmt_modified` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-  `mid` int(11) NOT NULL,
-  PRIMARY KEY  (`tx_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-SET FOREIGN_KEY_CHECKS=1;
-
-SET NAMES utf8;
-SET FOREIGN_KEY_CHECKS=0;
--- ----------------------------
--- Table structure for txc_branch_log
--- ----------------------------
-CREATE TABLE `txc_branch_log` (
-  `branch_id` bigint(20) NOT NULL auto_increment,
-  `tx_id` bigint(20) NOT NULL,
-  `client_app_name` varchar(50) collate utf8_bin NOT NULL,
-  `client_ip` varchar(15) collate utf8_bin NOT NULL,
-  `client_info` varchar(200) collate utf8_bin NOT NULL,
-  `udata` varchar(200) collate utf8_bin default NULL,
-  `commit_mode` tinyint(1) unsigned zerofill NOT NULL,
-  `state` tinyint(1) NOT NULL,
-  `gmt_created` datetime NOT NULL,
-  `gmt_modified` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-  `mid` int(11) NOT NULL,
-  PRIMARY KEY  (`branch_id`),
-  KEY `tx_id` (`tx_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-SET FOREIGN_KEY_CHECKS=1;
-
-SET NAMES utf8;
-SET FOREIGN_KEY_CHECKS=0;
--- ----------------------------
--- Table structure for txc_branch_error_log
--- ----------------------------
 CREATE TABLE `txc_branch_error_log` (
   `branch_id` bigint(20) NOT NULL,
   `tx_id` bigint(20) NOT NULL,
-  `client_app_name` varchar(50) collate utf8_bin NOT NULL,
-  `client_ip` varchar(15) collate utf8_bin NOT NULL,
-  `client_info` varchar(255) collate utf8_bin NOT NULL,
+  `client_app_name` varchar(50) COLLATE utf8_bin NOT NULL,
+  `client_ip` varchar(15) COLLATE utf8_bin NOT NULL,
+  `client_info` varchar(255) COLLATE utf8_bin NOT NULL,
   `commit_mode` tinyint(1) unsigned zerofill NOT NULL,
-  `state` tinyint(1) NOT NULL default '0',
+  `state` tinyint(1) NOT NULL DEFAULT '0',
   `gmt_created` datetime NOT NULL,
-  `gmt_modified` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-  `is_notify` tinyint(1) default '0',
+  `gmt_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `is_notify` tinyint(1) DEFAULT '0',
   `rt_sql` blob,
-  `mid` int(11) default NULL,
-  PRIMARY KEY  (`branch_id`),
+  `mid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`branch_id`),
   UNIQUE KEY `branch_id` (`branch_id`),
   KEY `tx_id` (`tx_id`),
-  KEY `client_app_name` USING BTREE (`client_app_name`),
+  KEY `client_app_name` (`client_app_name`) USING BTREE,
   KEY `commit_mode` (`commit_mode`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-SET FOREIGN_KEY_CHECKS=1;
 
+CREATE TABLE `txc_branch_log` (
+  `branch_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `tx_id` bigint(20) NOT NULL,
+  `client_app_name` varchar(200) COLLATE utf8_bin NOT NULL,
+  `client_ip` varchar(200) COLLATE utf8_bin NOT NULL,
+  `client_info` varchar(200) COLLATE utf8_bin NOT NULL,
+  `udata` varchar(200) COLLATE utf8_bin DEFAULT NULL,
+  `commit_mode` tinyint(1) unsigned zerofill NOT NULL,
+  `state` tinyint(1) NOT NULL,
+  `gmt_created` datetime NOT NULL,
+  `gmt_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `mid` int(11) NOT NULL,
+  PRIMARY KEY (`branch_id`),
+  KEY `tx_id` (`tx_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+CREATE TABLE `txc_global_log` (
+  `tx_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `state` tinyint(1) NOT NULL,
+  `gmt_created` datetime NOT NULL,
+  `gmt_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `mid` int(11) NOT NULL,
+  PRIMARY KEY (`tx_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
