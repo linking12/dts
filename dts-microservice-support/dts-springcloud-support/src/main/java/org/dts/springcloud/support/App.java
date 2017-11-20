ea3ea8f6742a047d96e68dc66c2007c235bb78c3
shiming.liu <shiming.liu@dianrong.com>
2017-11-20 12:01:42 +0800

    add spring cloud support

---
+++ dts-microservice-support/dts-springcloud-support/src/main/java/org/dts/springcloud/support/App.java
@@ -0,0 +1,13 @@
+package org.dts.springcloud.support;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
