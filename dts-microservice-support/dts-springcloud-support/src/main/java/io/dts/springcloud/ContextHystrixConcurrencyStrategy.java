fc5c03f6d67c0c8b70bf29cbccb26eba4b8377f3
shiming.liu <shiming.liu@dianrong.com>
2017-11-22 18:48:19 +0800

    update

--- dts-microservice-support/dts-springcloud-support/src/main/java/io/dts/springcloud/ContextHystrixConcurrencyStrategy.java
+++ dts-microservice-support/dts-springcloud-support/src/main/java/io/dts/springcloud/ContextHystrixConcurrencyStrategy.java
@@ -16,13 +16,34 @@ package io.dts.springcloud;
 import java.util.Map;
 import java.util.concurrent.Callable;
 
+import com.netflix.hystrix.strategy.HystrixPlugins;
 import com.netflix.hystrix.strategy.concurrency.HystrixConcurrencyStrategy;
+import com.netflix.hystrix.strategy.eventnotifier.HystrixEventNotifier;
+import com.netflix.hystrix.strategy.executionhook.HystrixCommandExecutionHook;
+import com.netflix.hystrix.strategy.metrics.HystrixMetricsPublisher;
+import com.netflix.hystrix.strategy.properties.HystrixPropertiesStrategy;
 
 /**
  * @author liushiming
  * @version HystrixConcurrencyStrategy.java, v 0.0.1 2017年11月22日 下午5:58:46 liushiming
  */
 public class ContextHystrixConcurrencyStrategy extends HystrixConcurrencyStrategy {
+
+
+  public ContextHystrixConcurrencyStrategy() {
+    HystrixCommandExecutionHook commandExecutionHook =
+        HystrixPlugins.getInstance().getCommandExecutionHook();
+    HystrixEventNotifier eventNotifier = HystrixPlugins.getInstance().getEventNotifier();
+    HystrixMetricsPublisher metricsPublisher = HystrixPlugins.getInstance().getMetricsPublisher();
+    HystrixPropertiesStrategy propertiesStrategy =
+        HystrixPlugins.getInstance().getPropertiesStrategy();
+    HystrixPlugins.reset();
+    HystrixPlugins.getInstance().registerConcurrencyStrategy(this);
+    HystrixPlugins.getInstance().registerCommandExecutionHook(commandExecutionHook);
+    HystrixPlugins.getInstance().registerEventNotifier(eventNotifier);
+    HystrixPlugins.getInstance().registerMetricsPublisher(metricsPublisher);
+    HystrixPlugins.getInstance().registerPropertiesStrategy(propertiesStrategy);
+  }
 
   @Override
   public <T> Callable<T> wrapCallable(Callable<T> callable) {
